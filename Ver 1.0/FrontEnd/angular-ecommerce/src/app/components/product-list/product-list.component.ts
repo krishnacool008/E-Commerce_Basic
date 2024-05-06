713b7a3ab346b028767e51dc98c484fa28760452
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2024-05-07 01:26:27 +0530

    changed bootstrap version to 4.4.1

--- Ver 1.0/FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Ver 1.0/FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -19,7 +19,7 @@ export class ProductListComponent implements OnInit {
 
   //new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 4;
+  thePageSize: number = 8;
   theTotalElements: number = 0;
 
   previousKeyword: string = "";
