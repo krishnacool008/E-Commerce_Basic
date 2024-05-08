1185a220e1f5c59c6ee1a08f97870568ff55924a
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2024-05-08 19:42:54 +0530

    Setup country and state drop down done

--- Ver 1.0/FrontEnd/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Ver 1.0/FrontEnd/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
@@ -16,6 +18,11 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] =[];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
   
   constructor(private formBuilder: FormBuilder, private luv2ShopFormService: Luv2ShopFormService) { }
 
@@ -69,6 +76,14 @@ export class CheckoutComponent implements OnInit {
         this.creditCardYears = data;
       }
     );
+
+    //populate counties
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: "+ JSON.stringify(data));
+        this.countries = data;
+      }
+    )
   }
 
   copyShippingAddressToBillingAddress(event) {
@@ -76,9 +91,16 @@ export class CheckoutComponent implements OnInit {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress']
             .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
+      //State array is empty cuz we don't select contry manualy when we copy address
+      this.billingAddressStates = this.shippingAddressStates;
+
     }
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      //Removing the elements when we uncheck the copy option
+      this.billingAddressStates = []
     }
   }
 
@@ -86,6 +108,9 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The shipping address country is "+ this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is "+ this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   handleMonthsAndYears() {
@@ -111,5 +136,28 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
     )
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName == 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }else{
+          this.billingAddressStates = data;
+        }
+
+        //select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
   }
 }
--- Ver 1.0/FrontEnd/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ Ver 1.0/FrontEnd/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,12 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statessUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
 
@@ -37,5 +43,33 @@ export class Luv2ShopFormService {
     }
 
     return of(data);
+  }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+
+    //search url
+    const searchUrl = `${this.statessUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
+}
+
+interface GetResponseCountries{
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates{
+  _embedded: {
+    states: State[];
   }
 }
