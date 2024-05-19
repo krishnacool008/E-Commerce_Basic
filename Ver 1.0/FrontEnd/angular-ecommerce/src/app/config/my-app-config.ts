12166e842d16322661966fa7532c0ac951cd01e9
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2024-05-19 18:02:59 +0530

    Secured the app with "Https"

--- Ver 1.0/FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
+++ Ver 1.0/FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oah4io7q7vYlbUun5d7',
         issuer: 'https://dev-79548037.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
